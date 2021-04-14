66cc65646cb43a1c4270c9d7588cc086851246b6
jmendozacr <mendoza077@gmail.com>
2021-04-14 15:13:47 -0600

    adding uuid to use id's and creating the tasks form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
+import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 
 const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: uuidv4(),
+            text: "go to the store",
+            complete: false
+        },
+        {
+            id: uuidv4(),
+            text: "go to the work",
+            complete: false
+        },
+        {
+            id: uuidv4(),
+            text: "do my homework",
+            complete: true
+        }
+    ]);
+
+    console.log("tasks", tasks);
+
     return (
         <div className="container">
             <Header/>
+            <Form tasks={tasks} setTasks={setTasks}/>
         </div>
     );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { v4 as uuidv4 } from 'uuid';
+
+const Form = ({ tasks, setTasks }) => {
+    const [value, setValue] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        setTasks([
+            ...tasks,
+            {
+                id: uuidv4(),
+                text: value,
+                complete: false
+            }
+        ])
+    }
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    return (
+        <form action="" className="task-form" onSubmit={onSubmit}>
+            <input
+                type="text"
+                className="task-form__input"
+                placeholder="write your task"
+                value={value}
+                onChange={(e) => handleChange(e)}
+            />
+            <button type="submit" className="task-form__btn">
+                <FontAwesomeIcon className="task-form__icon-btn" icon={faPlusSquare}/>
+            </button>
+        </form>
+    )
+}
+
+Form.propTypes = {
+    tasks: PropTypes.array,
+    setTasks: PropTypes.func,
+}
+
+export default Form;
