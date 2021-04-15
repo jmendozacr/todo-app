c072a018edb6c0683b9cb80e3abf80c68e68abd7
jmendozacr <mendoza077@gmail.com>
2021-04-14 21:16:46 -0600

    show and hide edit form

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckSquare, faEdit, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const Task = ({ item }) => {
+    const [editingTask, setEditingTask] = useState(false);
+    const [newTask, setNewTask] = useState(item.text);
+
+    const handleSubmitTask = (e) => {
+        e.preventDefault();
+        setEditingTask(false);
+    }
+
     return (
         <li className="list__task">
             <FontAwesomeIcon 
@@ -11,10 +19,23 @@ const Task = ({ item }) => {
                 icon={faCheckSquare}
             />
             <div className="list__text">
-                {item.text}    
+                {
+                    editingTask ?
+                    <form onSubmit={handleSubmitTask} action="" className="form-edit-task">
+                        <input 
+                            type="text"
+                            className="form-edit-task__input"
+                            value={newTask}
+                            onChange={(e) => setNewTask(e.target.value)}
+                        />
+                        <button type="submit" className="form-edit-task__btn">Update</button>
+                    </form>
+                    : item.text
+                }
             </div>
             <div className="list__container-buttons">
-                <FontAwesomeIcon 
+                <FontAwesomeIcon
+                    onClick={() => setEditingTask(!editingTask)}
                     className="list__icon list__icon-action"
                     icon={faEdit}
                 />
